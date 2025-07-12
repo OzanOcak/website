34b4d2740e484740c77abae78d64e8f219fb1449
OzanOcak <ozanocak@gmail.com>
2025-07-12 11:51:56 -0400

    dev website

--- frontend/src/app/(public)/apps/smartwords/page.tsx
+++ frontend/src/app/(public)/apps/smartwords/page.tsx
@@ -160,7 +160,7 @@ const App = () => {
           rel="noopener noreferrer"
           className="bg-white text-blue-700 font-bold py-4 px-10 rounded-full text-xl md:text-2xl shadow-lg hover:bg-gray-100 transition duration-300 transform hover:scale-105"
         >
-          Download SmartWords Today!
+          Download SmartWords
         </a>
       </section>
 
